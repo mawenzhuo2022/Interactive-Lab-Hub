34a3c4f795c4d4065b5022dfe78d66b101a51966
mawenzhuo2022 <mawenzhuo2022@gmail.com>
2025-10-28 21:08:19 -0400

    Minor updates to .venv config and greets script

--- Lab 3/greets.sh
+++ Lab 3/greets.sh
@@ -1,1 +1,3 @@
-echo "Hi! This is Wenzhuo! How are you?" | festival --tts
+# echo "Hi! This is Wenzhuo! How are you?" | festival --tts
+echo "You have been sitting for more than 30 minutes, please get up and move around" | festival --tts
+
